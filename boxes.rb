681aea0702f5e36540e4bc0cf27c2576db5afe5a
Varlog <varlog.err@gmail.com>
2018-08-05 00:35:04 +0300

    add debian boxes

--- boxes.rb
+++ boxes.rb
@@ -2,6 +2,8 @@
 # vi: set ft=ruby :
 
 BOX_ALIASES = {
+  "debian8serv"  => "bento/debian-8.11:201807.12.0",
+  "debian9serv"  => "bento/debian-9.5:201807.12.0",
   "ubuntu18serv" => "bento/ubuntu-18.04:201807.12.0",
   "ubuntu16serv" => "bento/ubuntu-16.04:201807.12.0",
   "ubuntu14serv" => "bento/ubuntu-14.04:201807.12.0",
